3ff509535fb8e1ed1265d02b665a220351b31163
Domin <dominus.the.creator@gmail.com>
2023-04-26 17:03:25 +0300

    update typing for reserialize

--- src/utils/api/reserialize.ts
+++ src/utils/api/reserialize.ts
@@ -1,4 +1,4 @@
 /** Reserialize the object by fixing getStaticProps issue (may be temporary) */
-export function reserialize(obj: any) {
+export function reserialize<T>(obj: T): T {
   return JSON.parse(JSON.stringify(obj));
 }
